746e703d0794a9b0519d21ee5a8caca8aef5943b
Aaron Joyce <joyceaa@tcd.ie>
2016-11-19 23:47:24 +0100

    Update chat_room.py

--- chat_room.py
+++ chat_room.py
@@ -13,13 +13,15 @@ class ChatRoom:
 	def register_observer(self, observer):
 		worker_identifier = len(self.observers)
 		self.observers[worker_identifier] = observer
-#		observer.broadcast(
-#			"JOINED_CHATROOM: {0}\nSERVER_IP: {1}\nPORT: {2}\nROOM_REF: {3}\nJOIN_ID: {4}\n".format(
-#				self.get_name(), 
-#				self.get_host(), 
-#				self.get_port(), 
-#				self.get_identifier(), 
-#				worker_identifier))
+		observer.broadcast(
+			"JOINED_CHATROOM:{0}\nSERVER_IP: {1}\nPORT: {2}\nROOM_REF: {3}\nJOIN_ID: {4}\n".format(
+				self.get_name(), 
+				self.get_host(), 
+				self.get_port(), 
+				self.get_identifier(), 
+				worker_identifier))
+		self.relay("{0} has joined this chatroom.".format(observer.get_client_name()), observer)
+#		observer.disconnect()
 		return worker_identifier
 
 	def deregister_observer(self, observer):
@@ -41,4 +43,4 @@ class ChatRoom:
 		for key in self.observers:
 			# request that they relay the message to listening client
 			observer = self.observers[key]
-			observer.broadcast("CHAT: {0}\nCLIENT_NAME: {1}\nMESSAGE: {2}\n".format(self.get_identifier(), relayer.get_client_name(), message_content))
+			observer.broadcast("CHAT:{0}\nCLIENT_NAME:{1}\nMESSAGE:{2}\n".format(self.get_identifier(), relayer.get_client_name(), message_content))
