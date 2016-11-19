e01d86247d7c47c8fb2c83a3d68cb44259bdbbef
Aaron Joyce <joyceaa@tcd.ie>
2016-11-19 13:05:53 +0100

    Update worker_pool.py

--- worker_pool.py
+++ worker_pool.py
@@ -12,7 +12,8 @@ class WorkerPool:
 		self.buffer_size = buffer_size
 		self.listener = None
 		self.thread_pool = []
-		self.chat_rooms = {'machine-learning':ChatRoom('machine-learning', 1, self.host, self.port)}
+		self.chat_rooms = {'machine-learning':ChatRoom('machine-learning', 1, self.host, self.port), 
+			'computer-vision':ChatRoom('computer-vision', 1, self.host, self.port)}
 
 	def run(self):
 		self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,10 +36,13 @@ class WorkerPool:
 				if (action_name == 'JOIN_CHATROOM'):
 					chat_room_name = action_key_value[action_key_value.find(':')+1:].strip()
 					chat_room = self.chat_rooms[chat_room_name.strip()]
-					client_name = None
+					client_name_key_value = received_split[3]
+					client_name = client_name_key_value[client_name_key_value.find(':')+1:].strip() 
 					worker = Worker(addr[0], addr[1], conn, 1024, chat_room, client_name)
+					#chat_room_join_identifier = worker.register_with_chatroom(chat_room)
 					worker.start()
 					self.thread_pool.append(worker)
+					#self.socket.sendall("JOINED_CHATROOM: {0}\nSERVER_IP: {1}\nPORT: {2}\nROOM_REF: {3}\nJOIN_ID: {4}\n".format(chat_room_name, addr[0], addr[1], chat_room.get_identifier(), chat_room_join_identifier))
 			else:
 				print 'Oops, busy', addr
 
